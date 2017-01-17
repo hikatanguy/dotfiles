68f6a8e2aa93eb51757e0eb6444d11c54b3bfd00
hikatanguy <hikaruteam@gmail.com>
2017-01-17 10:25:33 +0100

    Adding i3 config files

---
+++ i3/lock.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+icon="$HOME/.i3/lock.png"
+tmpbg='/tmp/screen.png'
+scrot "$tmpbg"
+convert "$tmpbg" -scale 10% -scale 1000% "$tmpbg"
+convert "$tmpbg" "$icon" -gravity center -composite -matte "$tmpbg"
+#dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Stop
+i3lock -u -i "$tmpbg"
+rm "$tmpbg"
